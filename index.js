8cdc39866d88290ba4ef8ac5355fc2b8b27cc16b
Douglas Moreira <dougmoreira@Douglass-MBP.lan>
2023-12-09 22:20:19 -0500

    home route

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/
 
 
 const PORT = process.env.PORT || 3000
-app.get('/register-click', (req,res) =>{
+app.get('/', (req,res) =>{
   res.statusCode(200).send("Hello World!!");
 })
 app.post('/register-click', (req,res)=>{
